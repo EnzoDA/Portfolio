4c215a87f26809da6ac410d0a14df395e181bc2b
EnzoDA <enzodasilvaribeiro@gmail.com>
2023-04-12 16:51:18 +0200

    c est bon

--- js.js
+++ js.js
@@ -70,38 +70,6 @@ var TxtRotate = function(el, toRotate, period) {
   };
 
 
-  document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-  });
-
-  // Get the modal
-var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("btn_ml");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
   const navbar = document.querySelector('nav');
   const links = document.querySelectorAll('nav .nav_links ul li a,nav .nav_links ul .content a');
@@ -128,3 +96,35 @@ window.onclick = function(event) {
     }})
 
   
+    document.querySelector('#contact-form').addEventListener('submit', (e) => {
+      e.preventDefault();
+      e.target.elements.name.value = '';
+      e.target.elements.email.value = '';
+      e.target.elements.message.value = '';
+    });
+  
+    // Get the modal
+  var modal = document.getElementById("modal");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("btn_ml");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
